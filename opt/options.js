877c2fcfa39612960a7eafc068031a4087c9b9fa
Tim P <tpollandt@d120.de>
2018-01-07 22:08:44 +0100

    continued development

--- opt/options.js
+++ opt/options.js
@@ -1,22 +1,34 @@
+function recreateMenus() {
+  //browser.contextMenus.removeAll();
+  //createMenus();
+  browser.runtime.reload();
+}
+
 function saveOptions(e) {
   e.preventDefault();
   browser.storage.sync.set({
     toolbar_button_def_lr: document.querySelector("#toolbar_button_def_lr").value,
     tab_context_def_lrd: document.querySelector("#tab_context_def_lrd").value,
-    page_context_def_lrd: document.querySelector("#page_context_def_lrd").value
+    page_context_def_lrd: document.querySelector("#page_context_def_lrd").value,
+    toolsmen_def_lrd: document.querySelector("#toolsmen_def_lrd").value,
+    keyboard_def_lrd: document.querySelector("#keyboard_def_lrd").value
   });
+  //recreateMenus();
 }
 
 var defaultSettings = {
   toolbar_button_def_lr: "right",
   tab_context_def_lrd: "left",
-  page_context_def_lrd: "disabled"
+  page_context_def_lrd: "disabled",
+  toolsmen_def_lrd: "right",
+  keyboard_def_lrd: "right"
 };
 
 function clearOptions(e) {
   e.preventDefault();
   browser.storage.sync.set(defaultSettings);
   restoreOptions();
+  //recreateMenus();
 }
 
 function restoreOptions() {
@@ -33,6 +45,14 @@ function restoreOptions() {
     document.querySelector("#page_context_def_lrd").value = result.page_context_def_lrd || "disabled";
   }
 
+  function setTMenD_lrd(result) {
+    document.querySelector("#toolsmen_def_lrd").value = result.toolsmen_def_lrd || "right";
+  }
+
+  function setKeyD_lrd(result) {
+    document.querySelector("#keyboard_def_lrd").value = result.keyboard_def_lrd || "right";
+  }
+
   function onError(error) {
     console.log(`Error: ${error}`);
   }
@@ -40,14 +60,45 @@ function restoreOptions() {
   var getting_tb_b_d_lr = browser.storage.sync.get("toolbar_button_def_lr");
   getting_tb_b_d_lr.then(setTbBD_lr, onError);
 
-  var getting_t_cm_d_lr = browser.storage.sync.get("tab_context_def_lrd");
-  getting_t_cm_d_lr.then(setTCmD_lrd, onError);
+  var getting_t_cm_d_lrd = browser.storage.sync.get("tab_context_def_lrd");
+  getting_t_cm_d_lrd.then(setTCmD_lrd, onError);
 
-  var getting_p_cm_d_lr = browser.storage.sync.get("page_context_def_lrd");
-  getting_p_cm_d_lr.then(setPCmD_lrd, onError);
+  var getting_p_cm_d_lrd = browser.storage.sync.get("page_context_def_lrd");
+  getting_p_cm_d_lrd.then(setPCmD_lrd, onError);
+
+  var getting_t_men_d_lrd = browser.storage.sync.get("toolsmen_def_lrd");
+  getting_t_men_d_lrd.then(setTMenD_lrd, onError);
+
+  var getting_key_d_lrd = browser.storage.sync.get("keyboard_def_lrd");
+  getting_key_d_lrd.then(setKeyD_lrd, onError);
 
 }
 
 document.addEventListener("DOMContentLoaded", restoreOptions);
 document.querySelector("form").addEventListener("submit", saveOptions);
 document.querySelector("form").addEventListener("reset", clearOptions);
+
+/*
+ * depricated
+//i18n
+function i18n_string(obj, tag) {
+    var msg = tag.replace(/__(\w+)__/g, function(match, v1) {
+        return v1 ? browser.i18n.getMessage(v1) : '';
+    });
+
+    if(msg != tag) obj.innerHTML = msg;
+}
+
+function html_i18n() {
+    var page = document.getElementsByTagName('body');
+
+    for (var j = 0; j < page.length; j++) {
+        var obj = page[j];
+        var tag = obj.innerHTML.toString();
+
+        i18n_string(obj, tag);
+    }
+}
+
+//html_i18n();
+*/
--- src/background.js
+++ src/background.js
@@ -3,11 +3,14 @@ var lrd = 1;
 var defaultSettings = {
   toolbar_button_def_lr: "right",
   tab_context_def_lrd: "left",
-  page_context_def_lrd: "disabled"
+  page_context_def_lrd: "disabled",
+  toolsmen_def_lrd: "right",
+  keyboard_def_lrd: "right"
 };
 
 function checkStoredSettings(storedSettings) {
-  if (!storedSettings.toolbar_button_def_lr || !storedSettings.tab_context_def_lrd || !storedSettings.page_context_def_lrd) {
+  if (!storedSettings.toolbar_button_def_lr || !storedSettings.tab_context_def_lrd || !storedSettings.page_context_def_lrd ||
+      !storedSettings.toolsmen_def_lrd || !storedSettings.keyboard_def_lrd) {
     browser.storage.sync.set(defaultSettings);
   }
 }
@@ -19,12 +22,12 @@ function onCreated() {
   if (browser.runtime.lastError) {
     console.log(`Error: ${browser.runtime.lastError}`);
   } else {
-    console.log("Context menu entry created successfully.");
+    //console.log("Context menu entry created successfully.");
   }
 }
 
 function onSuccess(tab) {
-  console.log(`Created new tab here: ${tab.index}`);
+  //console.log(`Created new tab here: ${tab.index}`);
 }
 
 function onError(error) {
@@ -33,8 +36,8 @@ function onError(error) {
 
 function openTabs(tabs) {
   for (let tab of tabs) {
-    console.log(tab.index);
-    console.log(lrd);
+    //console.log(tab.index);
+    //console.log(lrd);
     if (lrd < 2){
       var NTHcreate = browser.tabs.create({index: ((lrd == 0) ? tab.index : (tab.index + 1))});
       NTHcreate.then(onSuccess, onError);
@@ -43,39 +46,67 @@ function openTabs(tabs) {
   lrd = 1;
 }
 
-browser.contextMenus.create({
-  id: "NTH-page",
-  title: browser.i18n.getMessage("NewTabHerePageContextMenu"),
-  contexts: ["all"]
-}, onCreated)
+function createMenus(){
 
-if(browser.contextMenus.ContextType.TAB)
-{
-  browser.contextMenus.create({
-    id: "NTH-tab",
-    title: browser.i18n.getMessage("NewTabHereTabContextMenu"),
-    contexts: [browser.contextMenus.ContextType.TAB]
-  }, onCreated)
-}
+  function createPageContextMenu(setting) {
+    if(setting.page_context_def_lrd != "disabled"){
+      browser.menus.create({
+        id: "NTH-page",
+        title: browser.i18n.getMessage("NewTabHerePageContextMenu"),
+        contexts: ["all"]
+      }, onCreated)
+    }
+  }
 
-browser.contextMenus.onClicked.addListener(
-  function(info, tab) {
-    switch (info.menuItemId) {
+  function createTabContextMenu(setting) {
+    if(setting.tab_context_def_lrd != "disabled"){
+      if(browser.menus.ContextType.TAB)
+      {
+        browser.menus.create({
+          id: "NTH-tab",
+          title: browser.i18n.getMessage("NewTabHereTabContextMenu"),
+          contexts: [browser.menus.ContextType.TAB]
+        }, onCreated)
+      }
+    }
+  }
+
+  function createFileMenuEntry(setting) {
+    if(setting.tab_context_def_lrd != "disabled"){
+      browser.menus.create({
+        id: "NTH-menu",
+        title: browser.i18n.getMessage("NewTabHereToolsMenu"),
+        contexts: ["tools_menu"],
+      }, onCreated);
+    }
+  }
+
+  var getting1 = browser.storage.sync.get("page_context_def_lrd");
+  getting1.then(createPageContextMenu, onError);
+
+  var getting2 = browser.storage.sync.get("tab_context_def_lrd");
+  getting2.then(createTabContextMenu, onError);
+
+  var getting3 = browser.storage.sync.get("toolsmen_def_lrd");
+  getting3.then(createFileMenuEntry, onError);
+
+
+  browser.menus.onClicked.addListener(
+    function(info, tab) {
+      switch (info.menuItemId) {
       case "NTH-page":
         function setLRD(pos){
           if(pos.page_context_def_lrd == "left"){
             lrd = 0;
-            openTabs([tab]);
           }
           if(pos.page_context_def_lrd == "right"){
             lrd = 1;
-            openTabs([tab]);
           }
           if(pos.page_context_def_lrd == "disabled"){
             lrd = 2;
-            openTabs([tab]);
           }
-          console.log(pos);
+          openTabs([tab]);
+          //console.log(pos);
         }
         var getting = browser.storage.sync.get("page_context_def_lrd");
         getting.then(setLRD, onError);
@@ -84,48 +115,81 @@ browser.contextMenus.onClicked.addListener(
         function setLRD(pos){
           if(pos.tab_context_def_lrd == "left"){
             lrd = 0;
-            openTabs([tab]);
           }
           if(pos.tab_context_def_lrd == "right"){
             lrd = 1;
-            openTabs([tab]);
           }
           if(pos.tab_context_def_lrd == "disabled"){
             lrd = 2;
-            openTabs([tab]);
           }
-          console.log(pos);
-          console.log(pos.tab_context_def_lrd);
+          openTabs([tab]);
+          //console.log(pos);
+          //console.log(pos.tab_context_def_lrd);
         }
         var getting = browser.storage.sync.get("tab_context_def_lrd");
         getting.then(setLRD, onError);
         break;
-  }
-})
+      case "NTH-menu":
+        function setLRD(pos){
+          if(pos.toolsmen_def_lrd == "left"){
+            lrd = 0;
+          }
+          if(pos.toolsmen_def_lrd == "right"){
+            lrd = 1;
+          }
+          if(pos.toolsmen_def_lrd == "disabled"){
+            lrd = 2;
+          }
+          openTabs([tab]);
+          //console.log(pos);
+          //console.log(pos.tab_context_def_lrd);
+        }
+        var getting = browser.storage.sync.get("toolsmen_def_lrd");
+        getting.then(setLRD, onError);
+        break;
+      }
+    })
 
-browser.commands.onCommand.addListener(function(command) {
-  if (command == "new-tab-here") {
-    var querying = browser.tabs.query({currentWindow: true, active: true});
-    querying.then(openTabs, onError);
-    console.log("Opening new tab (keyboard shortcut)");
   }
-});
 
-browser.browserAction.onClicked.addListener(function() {
-  function setLRD(pos){
-    if(pos.toolbar_button_def_lr == "left"){
-      lrd = 0;
+  createMenus();
+
+  browser.commands.onCommand.addListener(function(command) {
+    if (command == "new-tab-here") {
+      function setLRD(pos){
+        if(pos.keyboard_def_lrd == "left"){
+          lrd = 0;
+        }
+        if(pos.keyboard_def_lrd == "right"){
+          lrd = 1;
+        }
+        if(pos.keyboard_def_lrd == "disabled"){
+          lrd = 2;
+        }
+        var querying = browser.tabs.query({currentWindow: true, active: true});
+        querying.then(openTabs, onError);
+      }
+      var getting = browser.storage.sync.get("keyboard_def_lrd");
+      getting.then(setLRD, onError);
+      //console.log("Opening new tab (keyboard shortcut)");
     }
-    if(pos.toolbar_button_def_lr == "right"){
-      lrd = 1;
+  });
+
+  browser.browserAction.onClicked.addListener(function() {
+    function setLRD(pos){
+      if(pos.toolbar_button_def_lr == "left"){
+        lrd = 0;
+      }
+      if(pos.toolbar_button_def_lr == "right"){
+        lrd = 1;
+      }
+      var querying = browser.tabs.query({currentWindow: true, active: true});
+      querying.then(openTabs, onError);
+      //console.log(pos);
     }
-    var querying = browser.tabs.query({currentWindow: true, active: true});
-    querying.then(openTabs, onError);
-    console.log(pos);
-  }
 
-  var getting = browser.storage.sync.get("toolbar_button_def_lr");
-  getting.then(setLRD, onError);
+    var getting = browser.storage.sync.get("toolbar_button_def_lr");
+    getting.then(setLRD, onError);
 
-  console.log("Opening new tab (toolbar button)");
-});
+    //console.log("Opening new tab (toolbar button)");
+  });
